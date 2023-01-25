04609b665c73fa253146342e12821cdb43dc6aac
Svetlanahrhr <maadlana@mail.ru>
2023-01-25 11:47:52 +0400

    feat: add winners popup

--- src/App.ts
+++ src/App.ts
@@ -1,171 +1,112 @@
 import CarsPage from './pages/CarsPage';
 import MainPageView from './pages/MainPageView';
-import {selectCar, updateCar, createCar, deleteCar, startEngine, stopEngine, driveCar, animateSVG} from './main/actions'
+import {
+    selectCar,
+    updateCar,
+    createCar,
+    deleteCar,
+    startEngine,
+    stopEngine,
+    driveCar,
+    moveCar,
+} from './main/actions';
 
 interface IResult {
-  success?: boolean;
+    success?: boolean;
 }
-let test:number;
+let test: number;
 
 export default class App {
-  static container: HTMLElement;
-  static indexOfCar: number;
+    static container: HTMLElement;
+    static indexOfCar: number;
 
-  static async renderNewPage(hash:string){
-    if (hash === 'garage') {
-      App.container.innerHTML = ''
-      App.container.append(await new CarsPage().render())
-    } else if (hash === 'winners') {
-      App.container.innerHTML = 'winners page'
-    } else {
-    App.container.innerHTML = 'Page not found'
+    static async renderNewPage(hash: string) {
+        if (hash === 'garage') {
+            App.container.innerHTML = '';
+            App.container.append(await new CarsPage().render());
+        } else if (hash === 'winners') {
+            App.container.innerHTML = 'winners page';
+        } else {
+            App.container.innerHTML = 'Page not found';
+        }
     }
-  }
 
-  private routeChange() {
-    window.addEventListener('hashchange', () => {
-        const hash = window.location.hash.slice(1);
-        App.renderNewPage(hash);
-    });
-}
-  async run(){
-    new MainPageView().render();
-    const cars = new CarsPage().render();
-    App.container = document.querySelector('.container')
-    App.container.append(await cars)
-    this.routeChange();
-    this.eventsListener();
-  }
-  private eventsListener() {
-    document.body.addEventListener('click', async (event) => {
-      let target = event.target as HTMLElement;
-      console.log(event.target);
-
-      if (target.classList.contains('select')) {
-        App.indexOfCar = Number(target.classList[0].slice(6))
-        console.log(target.classList[0].slice(6));
-        (document.querySelector('.update') as HTMLButtonElement).disabled = false;
-        selectCar(App.indexOfCar)
-      }
-      if (target.classList.contains('update')) {
-        // document.querySelector('.container')
-        updateCar(App.indexOfCar)
-      }
-      if (target.classList.contains('create-btn')) {
-        const nameCar = (document.querySelector('#create') as HTMLInputElement).value;
-        console.log(nameCar);
-
-        const chooseColor = (document.querySelector('#createColor') as HTMLInputElement).value;
-        await createCar({
-          name: nameCar,
-          color: chooseColor,
+    private routeChange() {
+        window.addEventListener('hashchange', () => {
+            const hash = window.location.hash.slice(1);
+            App.renderNewPage(hash);
         });
-      }
-      if (target.classList.contains('delete')) {
-        const indexOfCar = Number((event.target as HTMLElement).classList[0].slice(6));
-        deleteCar(indexOfCar);
-      }
-      if (target.classList.contains('start')) {
-        const indexOfCar = Number(target.classList[0].slice(5));
-        let duration = startEngine(indexOfCar);
-        console.log(await duration);
-        let result: IResult = await driveCar(await duration,indexOfCar);
-        // let road = document.querySelector('.road') as HTMLDivElement;
-        let svg = document.querySelector(`.icon${indexOfCar}`) as HTMLElement;
-        console.log(svg.getBoundingClientRect().left);
+    }
+    async run() {
+        new MainPageView().render();
+        const cars = new CarsPage().render();
+        App.container = document.querySelector('.container');
+        App.container.append(await cars);
+        this.routeChange();
+        this.eventsListener();
+    }
+    private eventsListener() {
+        document.body.addEventListener('click', async (event) => {
+            let target = event.target as HTMLElement;
+            console.log(event.target);
 
+            if (target.classList.contains('select')) {
+                App.indexOfCar = Number(target.classList[0].slice(6));
+                console.log(target.classList[0].slice(6));
+                (document.querySelector('.update') as HTMLButtonElement).disabled = false;
+                selectCar(App.indexOfCar);
+            }
+            if (target.classList.contains('update')) {
+                // document.querySelector('.container')
+                updateCar(App.indexOfCar);
+            }
+            if (target.classList.contains('create-btn')) {
+                const nameCar = (document.querySelector('#create') as HTMLInputElement).value;
+                console.log(nameCar);
 
-        test = moveCar(svg, await duration, indexOfCar);
-        console.log(test);
+                const chooseColor = (document.querySelector('#createColor') as HTMLInputElement).value;
+                await createCar({
+                    name: nameCar,
+                    color: chooseColor,
+                });
+            }
+            if (target.classList.contains('delete')) {
+                const indexOfCar = Number((event.target as HTMLElement).classList[0].slice(6));
+                deleteCar(indexOfCar);
+            }
+            if (target.classList.contains('start')) {
+                const indexOfCar = Number(target.classList[0].slice(5));
+                let duration = await startEngine(indexOfCar);
+                let svg = document.querySelector(`.icon${indexOfCar}`) as HTMLElement;
+                test = moveCar(svg, duration, indexOfCar);
+                await driveCar(indexOfCar);
+            }
+            if (target.classList.contains('stop')) {
+              const indexOfCar = Number(target.classList[0].slice(4));
+              let svg = document.querySelector(`.icon${indexOfCar}`) as HTMLElement;
+              cancelAnimationFrame(test);
+              stopEngine(indexOfCar);
+              svg.style.transform = `translateX(${0}px)`;
+            }
+            if (document.querySelector('.popup')) {
+              if (!target.classList.contains('popup')) {
+                document.querySelector('.popup').classList.add('hide');
+              }
+            }
+            if (target.classList.contains('start-race')) {
+                const cars = document.querySelectorAll('.icon');
 
-        // let test = animateSVG(svg, await duration);
+                cars.forEach(async (car) => {
+                    let indexOfCar = Number(car.classList[0].slice(4));
+                    let duration = await startEngine(indexOfCar);
 
+                    let svg = document.querySelector(`.icon${indexOfCar}`) as HTMLElement;
+                    test = moveCar(svg, duration, indexOfCar);
+                    driveCar(indexOfCar);
+                });
 
-        if(!result.success){
-          stopEngine(indexOfCar);
-          cancelAnimationFrame(test);
-          clearInterval(test);
-          svg.style.transform = `translateX(${0}px)`;
-        }
-
-
-      }
-      if (target.classList.contains('stop')) {
-        const indexOfCar = Number(target.classList[0].slice(4));
-        let svg = document.querySelector(`.icon${indexOfCar}`) as HTMLElement;
-        cancelAnimationFrame(test);
-
-
-        stopEngine(indexOfCar);
-        svg.style.transform = `translateX(${0}px)`;
-        // clearInterval(test);
-
-      }
-
-      if (target.classList.contains('start-race')) {
-        const cars = document.querySelectorAll('.icon');
-
-        cars.forEach(async (car) => {
-          let indexOfCar= Number(car.classList[0].slice(4))
-          let duration = startEngine(indexOfCar);
-
-
-
-        let result: IResult = await driveCar(await duration,indexOfCar);
-        // let road = document.querySelector('.road') as HTMLDivElement;
-        let svg = document.querySelector(`.icon${indexOfCar}`) as HTMLElement;
-        console.log(svg.getBoundingClientRect().left);
-
-
-        test = moveCar(svg, await duration, indexOfCar);
-
-        if(!result.success){
-          stopEngine(indexOfCar);
-          cancelAnimationFrame(test);
-          clearInterval(test);
-          svg.style.transform = `translateX(${0}px)`;
-        }
-        })
-        // Promise.all()
-
-      }
-
-    })
-  }
+            }
+        });
+    }
 }
 
-function animateElement(element: HTMLElement, animationType:string, duration:number) {
-  element.style.animation = `${animationType} ${duration}s`;
-  }
-
-  function moveCar(svg: HTMLElement, duration:number,indexOfCar:number) {
-    let road = document.querySelector('.road') as HTMLDivElement;
-    let endX =road.getBoundingClientRect().width;
-    let currentX:number;
-    // let timer = setTimeout(() => {
-    // currentX = svg.getBoundingClientRect().left;
-    // console.log('currentX', currentX);
-    // }, duration);
-    // console.log('currentX', currentX);
-    currentX = svg.getBoundingClientRect().left;
-    const framesCount = (duration / 1000) * 60;
-    const dX = (endX - svg.getBoundingClientRect().left) / framesCount;
-    let test:number;
-    const tick = () => {
-      currentX += dX;
-
-      svg.style.transform = `translateX(${currentX}px)`;
-
-      if (currentX+100 < endX) {
-        test = requestAnimationFrame(tick);
-      }
-      document.querySelector(`.stop${indexOfCar}`).addEventListener('click', async () => {
-        // this.stopCar();
-        svg.style.transform = `translateX(${0}px)`;
-        cancelAnimationFrame(test);
-        // clearTimeout(timer);
-      });
-    };
-    tick();
-    return test;
-  }
--- src/main/actions.ts
+++ src/main/actions.ts
@@ -1,105 +1,135 @@
-import App from "../App";
-import CarsPage from "../pages/CarsPage";
-import MainPageView from "../pages/MainPageView";
+import App from '../App';
+import CreateElement from '../helpers/CreateElement';
+let test: number;
+let popup: HTMLElement;
+let winner: IWinner = {
+    id: 0,
+    duration: 0,
+};
 
-export  async function selectCar(indexOfCar:number) {
-  const resp = await fetch(`http://127.0.0.1:3000/garage/${indexOfCar}`);
-  const car = await resp.json();
-
-  (document.querySelector('#update') as HTMLInputElement).value = car.name;
-  (document.querySelector('.update-color') as HTMLInputElement).value = car.color;
-  document.querySelector(`.name${indexOfCar}`).classList.add('selected');
-
+interface IWinner {
+    id?: number;
+    duration?: number;
 }
 
-export  async function updateCar(id:number) {
-  const resp = await fetch(`http://127.0.0.1:3000/garage/${id}`, {
-    method: 'PUT',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      name: (document.querySelector('#update')as HTMLInputElement).value,
-      color: (document.querySelector('.update-color')as HTMLInputElement).value,
-    }),
-  });
-  await resp.json();
-  App.renderNewPage('garage')
-  return;
+export async function selectCar(indexOfCar: number) {
+    const resp = await fetch(`http://127.0.0.1:3000/garage/${indexOfCar}`);
+    const car = await resp.json();
+
+    (document.querySelector('#update') as HTMLInputElement).value = car.name;
+    (document.querySelector('.update-color') as HTMLInputElement).value = car.color;
+    document.querySelector(`.name${indexOfCar}`).classList.add('selected');
 }
 
-export async function createCar (body: object) {
-  const resp = await fetch(`http://127.0.0.1:3000/garage`, {
+export async function updateCar(id: number) {
+    const resp = await fetch(`http://127.0.0.1:3000/garage/${id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            name: (document.querySelector('#update') as HTMLInputElement).value,
+            color: (document.querySelector('.update-color') as HTMLInputElement).value,
+        }),
+    });
+    await resp.json();
+    App.renderNewPage('garage');
+    return;
+}
+
+export async function createCar(body: object) {
+    const resp = await fetch(`http://127.0.0.1:3000/garage`, {
         method: 'POST',
         headers: {
-              'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
         },
-        body: JSON.stringify(body)
-  })
-  App.renderNewPage('garage')
-  return;
+        body: JSON.stringify(body),
+    });
+    App.renderNewPage('garage');
+    return;
 }
 
-export async function deleteCar (id:number) {
-  const resp = await fetch(`http://127.0.0.1:3000/garage/${id}`, {
+export async function deleteCar(id: number) {
+    const resp = await fetch(`http://127.0.0.1:3000/garage/${id}`, {
         method: 'DELETE',
-  })
-  App.renderNewPage('garage')
-  return;
-}
-export async function startEngine(indexOfCar:number) {
-  const resp = await fetch(
-    `http://127.0.0.1:3000/engine?id=${indexOfCar}&status=started`,
-    {
-      method: 'PATCH',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        status: 'started',
-      }),
-    },
-  );
-  const r = await resp.json();
-  const { velocity, distance } = r;
-  const duration = Math.round(distance / velocity);
-
-  return duration;
+    });
+    App.renderNewPage('garage');
+    return;
 }
 
-export async function driveCar(duration: number, indexOfCar: number) {
-  let resp;
-    await fetch(`http://127.0.0.1:3000/engine?id=${indexOfCar}&status=drive`, { method: 'PATCH' }).then(res => {
-      if(res.ok){
-        resp = res.json();
-      } else {
-        resp = 'error';
-      }
-    }).catch(err => console.log(err)
-    )
+export async function startEngine(indexOfCar: number) {
+    const resp = await fetch(`http://127.0.0.1:3000/engine?id=${indexOfCar}&status=started`, {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            status: 'started',
+        }),
+    });
+    const r = await resp.json();
+    const { velocity, distance } = r;
+    const duration = Math.round(distance / velocity);
 
-    return resp;
-
+    return duration;
 }
 
-export async function stopEngine(indexOfCar:number) {
-  const resp = (
-    await fetch(`http://127.0.0.1:3000/engine?id=${indexOfCar}&status=stopped`, { method: 'PATCH' })
-  ).json();
-  console.log(await resp);
+export async function driveCar(indexOfCar: number) {
+    await fetch(`http://127.0.0.1:3000/engine?id=${indexOfCar}&status=drive`, { method: 'PATCH' })
+        .then((res) => {
+            if (!res.ok) {
+                cancelAnimationFrame(test);
+            }
+        })
+        .catch((err) => console.log(err));
 }
 
-export function animateSVG(svgElement: HTMLElement, speed: number){
-  let currentPosition = 0;
+export async function stopEngine(indexOfCar: number) {
+    fetch(`http://127.0.0.1:3000/engine?id=${indexOfCar}&status=stopped`, { method: 'PATCH' });
 
-  // animate the SVG element with a setInterval
-  let animInterval = setInterval(() => {
-  // increase the current position
-  currentPosition += speed;
-  // apply the current position to the svg element
-  svgElement.style.transform = `translateX(${currentPosition}px)`;
-  }, 16);
+    let svg = document.querySelector(`.icon${indexOfCar}`) as HTMLElement;
+    cancelAnimationFrame(test);
+    svg.style.transform = `translateX(${0}px)`;
+    clearInterval(test);
+}
 
-  // return the animation interval so it can be cleared if necessary
-  return animInterval;
-  }
+export function moveCar(svg: HTMLElement, duration: number, indexOfCar: number) {
+    let road = document.querySelector('.road') as HTMLDivElement;
+    let endX = road.getBoundingClientRect().width;
+    let currentX: number;
+    currentX = svg.getBoundingClientRect().left;
+    const framesCount = (duration / 1000) * 60;
+    const dX = (endX - svg.getBoundingClientRect().left) / framesCount;
+    const tick = () => {
+        currentX += dX;
+
+        svg.style.transform = `translateX(${currentX}px)`;
+
+        if (currentX + 100 < endX) {
+            test = requestAnimationFrame(tick);
+        } else {
+            if (winner.id === 0) {
+                winner.id = indexOfCar;
+                winner.duration = duration;
+                showWinner(winner);
+            }
+        }
+    };
+    winner.id = 0;
+    tick();
+    return test;
+}
+
+function showWinner(winner: IWinner) {
+    if (document.querySelector('.popup')) {
+        document.querySelector('.popup').classList.remove('hide');
+        document.querySelector('.popup').innerHTML = `Winner is №${winner.id} duration is ${winner.duration}`;
+    } else {
+        popup = new CreateElement(
+            'div',
+            `Winner is №${winner.id} duration is ${winner.duration}`,
+            'popup'
+        ).getElement();
+        document.querySelector('.container').append(popup);
+    }
+}
